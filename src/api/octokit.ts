209e2ec508f01ff70f4917c61d4f5728e6d9f1ef
Patata <patattta93@gmail.com>
2023-09-01 22:10:22 +0900

    feat: remove .env values

--- src/api/octokit.ts
+++ src/api/octokit.ts
@@ -3,8 +3,7 @@ import { Octokit } from 'octokit';
 import { Issue } from '@/context/IssueListProvider';
 
 export const octokit = new Octokit({
-  auth: process.env.NEXT_PUBLIC_OCTOKIT_TOKEN,
-  baseUrl: process.env.NEXT_PUBLIC_BASE_API_URL,
+  baseUrl: 'https://api.github.com/repos/facebook/react',
 });
 
 export const getIssuesPage = async (pageParam = 1) => {
